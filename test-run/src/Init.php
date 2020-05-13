16a0ead30dfc7be1e4fe746c5187beb2e010e474
Chanaka Abeysinghe <chanakasan@gmail.com>
2020-05-13 19:25:56 +0530

    render admin page using timber

--- test-run/src/Init.php
+++ test-run/src/Init.php
@@ -2,6 +2,8 @@
 
 namespace TestRun;
 
+use Timber\Timber;
+
 /**
  * We can encapsulated our plugin code inside a class like below.
  * This will prevent us from adding things to global namespace.
@@ -15,12 +17,18 @@ class Init
 
   protected function init()
   {
-    add_action('admin_notices', [$this, 'show_message_cb']);
+    add_action('admin_menu', [$this, 'admin_menu_cb']);
   }
 
-  public function show_message_cb()
+  public function admin_menu_cb()
   {
-    $message = "Yay! Test Run plugin is active. But it's empty :(";
-    printf('<div class="error notice"><h2/>%s</h2></div>', $message);
+    // Ref: https://developer.wordpress.org/reference/functions/add_menu_page/
+    add_menu_page('Test Run Admin Page', 'Test Run', 'manage_options', 'test-run', [$this, 'render_menu_page'], 'dashicons-schedule', 3);
+  }
+
+  public function render_menu_page()
+  {
+    Timber::$locations = __DIR__.'/views';
+    Timber::render('main.twig');
   }
 }
