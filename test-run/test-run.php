1d25d26dc6769e25885b664f7724d1d0bb133655
Chanaka Abeysinghe <chanakasan@gmail.com>
2020-05-13 20:24:27 +0530

    define plugin file path

--- test-run/test-run.php
+++ test-run/test-run.php
@@ -4,6 +4,15 @@
  * Plugin Name: Test Run
  */
 
+
+// don't run if someone access this file directly
+defined('ABSPATH') || exit;
+
+// define plugin file path
+if (!defined('TEST_RUN_PLUGIN_FILE')) {
+  define('TEST_RUN_PLUGIN_FILE', __FILE__);
+}
+
 // We require the composer autoloader
 if (file_exists($composer_autoload = __DIR__.'/vendor/autoload.php')) {
   require_once $composer_autoload;
