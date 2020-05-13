f054e38b206b1a26005133abc3be5b3241301430
Chanaka Abeysinghe <chanakasan@gmail.com>
2020-05-13 20:52:33 +0530

    implement tabs for admin page

--- test-run/src/View.php
+++ test-run/src/View.php
@@ -9,6 +9,13 @@ class View
   public static function render_menu_page()
   {
     Timber::$locations = __DIR__.'/views';
-    Timber::render('main.twig');
+
+    $tab = intval($_GET['tab'] ?? '1');
+    $data = [
+      'page_name' => 'test-run',
+      'current_tab' => $tab,
+      'current_tab_partial' => "tab_$tab.twig",
+    ];
+    Timber::render('main.twig', $data);
   }
 }
